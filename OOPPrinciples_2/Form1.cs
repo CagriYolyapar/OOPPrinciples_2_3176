ae8e6da2ec010ae38f6016116fc0be1913f03e00
cagri.yolyapar <cagri.yolyapar@gmail.com>
2024-11-19 14:20:22 +0300

    Add project files.

---
+++ OOPPrinciples_2/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOPPrinciples_2
+{
+    public partial class Form1 : Form
+    {
+
+        #region Prensipler
+
+        /*
+         
+                OOP felsefesinin tam anlamıyla uygulanabilmesi icin sadece class acmak (hatta OOP'yi destekleyen diger yapıları da acmak) bize yetmez...
+
+        OOP felsefesinin bize sundugu avantajları (extendibility(genişletilebilirlik), reusability(tekrar kullanılabilirlik), maintainability(sürdürülebilirlik)) gerçekleştirebilmesi icin icerisinde 4 temel prensip , 5 Advanced Prensip vardır...
+
+        Temel Prensipler
+        Encapsulation(Kapsülleme)
+        Inheritance(Miras alma/verme)
+        Polymorphism(Cok bicimlilik)
+        Abstraction (Veri Soyutlama)
+
+
+        Advanced Prensipler(SOLID Prensipler)
+        S => Single Responsibility(TEkil Sorumluluk)
+        O => Open Closed (Acık Kapalı)
+        L => Liskov's Substitution (Liskov'un Yerine Gecme Prensibi)
+        I => Interface Segregation (Interface Ayrımı)
+        D => Dependency Inversion (Bagımlılık Gevseklestirilmesi)
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         */
+
+
+
+
+
+
+
+
+        #endregion
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
